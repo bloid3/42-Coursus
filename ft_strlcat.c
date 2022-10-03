7d29dad63042f755a6c909ed30cbf3ebb559ba51
bloid3 <pablo.pereira00@gmail.com>
2022-10-03 21:28:45 +0200

    COmmit

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,31 +6,26 @@
 /*   By: papereir <papereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 12:49:26 by papereir          #+#    #+#             */
-/*   Updated: 2022/10/03 19:51:46 by papereir         ###   ########.fr       */
+/*   Updated: 2022/10/03 21:23:37 by papereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dest, const char *src, size_t size)
-{
-	size_t	i;
-	size_t	j;
-	size_t	contador;
-
-	i = ft_strlen(src);
-	j = ft_strlen(dest);
-	contador = 0;
-	if (size < j)
-		return (i + size);
-	else if (size > 0)
-	{
-		while (src[contador] && contador + j < size - 1)
-		{
-			dest[j + contador] = src[contador];
-			contador++;
-		}
-	}
-	dest[j + contador] = '\0';
-	return (i + j);
-}
+size_t	ft_strlcat(char *dest, const char *src, size_t n)
+ {
+         size_t        srcsize;
+         size_t        destsize;
+         if (!dest && !src)
+                 return (0);
+         srcsize = ft_strlen(src);
+         destsize = ft_strlen(dest);
+         if (n < destsize)
+                 return (srcsize + n);
+         else
+         {
+                 dest = dest + destsize;
+                 ft_strlcpy(dest, src, n - destsize);
+                 return (srcsize + destsize);
+         }
+ }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: papereir <papereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 12:55:46 by papereir          #+#    #+#             */
-/*   Updated: 2022/10/03 19:28:18 by papereir         ###   ########.fr       */
+/*   Updated: 2022/10/03 21:23:48 by papereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int		ft_memcmp(const void	*ptr1, const void	*ptr2, size_t num);
 void	*ft_memcpy(void	*dest, const void	*src, size_t n);
 void	*ft_memmove(void	*str1, const void	*str2, size_t n);
 char	*ft_strchr(const char	*str, int c);
-size_t	ft_strlcat(char	*dst, const char	*src, size_t size);
+size_t	ft_strlcat(char	*dest, const char	*src, size_t n);
 size_t	ft_strlcpy(char	*dst, const char	*src, size_t size);
 int		ft_strncmp(const char	*str1, const char *str2, size_t n);
 char	*ft_strnstr(const char	*big, const char	*little, size_t len);
